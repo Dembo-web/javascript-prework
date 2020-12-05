47f1cbb0a9b5a7989bfb5d289497dd1afe34c7ce
Jakub Debowski <karvis95@gmail.com>
2020-12-05 02:01:52 +0100

    Add player move

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
-  computerMove = 'kamień';
+  computerMove = 'kamien';
 } else if(randomNumber == 2) {
   computerMove = 'papier';
 } else {
@@ -13,3 +13,21 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamien, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamien';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nozyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
